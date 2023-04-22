a0ecc3aee428063836d7d142abcaadd253f6242f
xprilion <xprilion@gmail.com>
2023-04-22 20:57:23 +0530

    Add correct footer link

--- src/components/footerSection.js
+++ src/components/footerSection.js
@@ -4,7 +4,7 @@ import '@picocss/pico';
 function FooterSection() {
     return (
         <footer className="container">
-            <small>Built with ♥ by <a href="https://github.com/xprilion">Anubhav</a> • <a href="https://github.com/xprilion/crowd-ml-demo">Source code</a></small>
+            <small>Built with ♥ by <a href="https://github.com/xprilion">Anubhav</a> • <a href="https://github.com/xprilion/polling-app-demo">Source code</a></small>
         </footer>
     );
 }
